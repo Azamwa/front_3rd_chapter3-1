ffb58dc710d3c9ace24a788b9b73b14cadaf4cb2
이동아 <christar99@naver.com>
2024-11-04 23:38:32 +0900

    ✅ test: parseDateTime 함수수정,  convertEventToDateRange 함수 테스트

--- src/__tests__/unit/easy.eventOverlap.spec.ts
+++ src/__tests__/unit/easy.eventOverlap.spec.ts
@@ -15,29 +15,107 @@ describe('parseDateTime', () => {
   it('잘못된 날짜 형식에 대해 Invalid Date를 반환한다', () => {
     const dateTime = parseDateTime('2024/09/01', '14:30');
     expect(dateTime.toString()).toBe('Invalid Date');
+    expect(dateTime).toEqual(new Date(''));
   });
 
   it('잘못된 시간 형식에 대해 Invalid Date를 반환한다', () => {
     const dateTime = parseDateTime('2024-09-01', '14-30');
     expect(dateTime.toString()).toBe('Invalid Date');
+    expect(dateTime).toEqual(new Date(''));
   });
 
   it('날짜 문자열이 비어있을 때 Invalid Date를 반환한다', () => {
     const dateTime = parseDateTime('', '14:30');
     expect(dateTime.toString()).toBe('Invalid Date');
+    expect(dateTime).toEqual(new Date(''));
   });
 });
 
 describe('convertEventToDateRange', () => {
-  it('일반적인 이벤트를 올바른 시작 및 종료 시간을 가진 객체로 변환한다', () => {});
+  it('일반적인 이벤트를 올바른 시작 및 종료 시간을 가진 객체로 변환한다', () => {
+    const eventTime = convertEventToDateRange({
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2024-11-20',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: {
+        type: 'none',
+        interval: 0,
+      },
+      notificationTime: 1,
+    });
 
-  it('잘못된 날짜 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {});
+    expect(eventTime).toEqual({
+      start: new Date('2024-11-20T10:00'),
+      end: new Date('2024-11-20T11:00'),
+    });
+  });
 
-  it('잘못된 시간 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {});
+  it('잘못된 날짜 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {
+    const eventTime = convertEventToDateRange({
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2024/11/20',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: {
+        type: 'none',
+        interval: 0,
+      },
+      notificationTime: 1,
+    });
+    expect(eventTime.start.toString()).toBe('Invalid Date');
+    expect(eventTime.end.toString()).toBe('Invalid Date');
+    expect(eventTime).toEqual({
+      start: new Date(''),
+      end: new Date(''),
+    });
+  });
+
+  it('잘못된 시간 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {
+    const eventTime = convertEventToDateRange({
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2024-11-20',
+      startTime: '10-00',
+      endTime: '11-00',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: {
+        type: 'none',
+        interval: 0,
+      },
+      notificationTime: 1,
+    });
+    expect(eventTime.start.toString()).toBe('Invalid Date');
+    expect(eventTime.end.toString()).toBe('Invalid Date');
+    expect(eventTime).toEqual({
+      start: new Date(''),
+      end: new Date(''),
+    });
+  });
 });
 
 describe('isOverlapping', () => {
-  it('두 이벤트가 겹치는 경우 true를 반환한다', () => {});
+  it('두 이벤트가 겹치는 경우 true를 반환한다', () => {
+    // title: string;
+    // date: string;
+    // startTime: string;
+    // endTime: string;
+    // description: string;
+    // location: string;
+    // category: string;
+    // repeat: RepeatInfo;
+    // notificationTime: number;
+  });
 
   it('두 이벤트가 겹치지 않는 경우 false를 반환한다', () => {});
 });
